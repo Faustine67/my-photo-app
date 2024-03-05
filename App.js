a7f7b11009272db13b4f3f748d9444f1a9631034
Faustine67 <michelfaustine@gmail.com>
2024-03-05 09:53:18 +0100

    create a modal

--- App.js
+++ App.js
@@ -19,26 +19,35 @@ export default function App() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert('You did not select any image.');
     }
   };
-  return (
-    <View style={styles.container}>
-     <View style={styles.imageContainer}>
-     <ImageViewer 
-     placeholderImageSource={PlaceholderImage} 
-     selectedImage={selectedImage}
-     />
+    return (
+      <View style={styles.container}>
+      <View style={styles.imageContainer}>
+      <ImageViewer 
+      placeholderImageSource={PlaceholderImage} 
+      selectedImage={selectedImage}
+      />
+      {showAppOptions ? (
+        <View/>
+      ) : (
+      <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}  />
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>
       </View>
-     <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}  />
-        <Button label="Use this photo" />
-     </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+      )}
+        <StatusBar style="auto" />
+      <View/>
+    );
 }
+
+const [showAppOptions, setShowAppOptions] = useState(false);
+
+
+
 
 const styles = StyleSheet.create({
   container: {
